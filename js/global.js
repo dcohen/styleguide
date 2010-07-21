66b4d46d31b8dacb70ce317dbdbcaef2b71196fd
Chris Cooper <Chris@Chris-Coopers-MacBook.local>
2010-07-21 13:17:15 -0400

    Fancybox javascript

--- js/global.js
+++ js/global.js
@@ -1,38 +1,88 @@
-$('div.carousel a').live('click',
-	function() {					
-	var currentSpotlight = $(this).attr('rel');
-	var lastSpotlight = $(this).parents().find('a.carouselItem:last').attr('rel');
+$(document).ready(function()
+{
+// FancyBox Setup
+	/* This is basic - uses default settings */
 
-	$(this).parents().find('a.carouselItem[rel='+currentSpotlight+']').addClass('current');
-	$(this).parents().find('a.carouselItem[rel!='+currentSpotlight+']').removeClass('current');
+	/* Using custom settings */
 
-	$('img.bgHeroSlide'+currentSpotlight+'').siblings('img.bgHero').fadeOut(600);
-	$(this).parents().find('img.bgHeroSlide'+currentSpotlight+'').fadeIn(300);
-
-	$('img.heroLeftOverlay'+currentSpotlight+'').siblings().removeClass('current').fadeOut(300);
-	$(this).parents().find('img.heroLeftOverlay'+currentSpotlight+'').fadeIn(300);
-
-	$('p.heroCopy'+currentSpotlight+'').siblings('p').removeClass('current').fadeOut(600);
-	$(this).parents().find('p.heroCopy'+currentSpotlight+'').addClass('current').fadeIn(600);	
+	$("a.photoGallery").fancybox({
+		'type'				: 'iframe',
+		'hideOnContentClick': false,
+		'width'         	: 525,
+		'height'        	: 510,
+		'scrolling'			: 'no',
+		'autoDimensions'	: false,
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'overlayColor'		: '#000',
+		'padding'			: 0,
+		'overlayOpacity'	: 0.7,
+		'onComplete'		: function() { }
+	});
 	
-	// change the next/prev				
-	var prev = 1;
-	var next = 1;
-	if(currentSpotlight == lastSpotlight)
-		next = -lastSpotlight+1;
-	if(currentSpotlight == '1')
-		prev = -lastSpotlight+1;
+	$("a.youtube").fancybox({
+		'type'				: 'swf',
+		'hideOnContentClick': false,
+		'width'         	: 615,
+		'height'        	: 370,
+		'scrolling'			: 'no',
+		'autoDimensions'	: false,
+		'transitionIn'		: 'elastic',
+		'transitionOut'		: 'elastic',
+		'overlayColor'		: '#000',
+		'padding'			: 0,
+		'overlayOpacity'	: 0.7,
+		'onComplete'		: function() { }
+	});
+	/* Apply fancybox to multiple items */
+
+// END: FancyBox
+
+
+	$('div.carousel a').live('click',
+		function() {					
+		var currentSpotlight = $(this).attr('rel');
+		var lastSpotlight = $(this).parents().find('a.carouselItem:last').attr('rel');
+	
+		$(this).parents().find('a.carouselItem[rel='+currentSpotlight+']').addClass('current');
+		$(this).parents().find('a.carouselItem[rel!='+currentSpotlight+']').removeClass('current');
+	
+		$('img.bgHeroSlide'+currentSpotlight+'').siblings('img.bgHero').fadeOut(600);
+		$(this).parents().find('img.bgHeroSlide'+currentSpotlight+'').fadeIn(300);
+	
+		$('img.heroLeftOverlay'+currentSpotlight+'').siblings().removeClass('current').fadeOut(300);
+		$(this).parents().find('img.heroLeftOverlay'+currentSpotlight+'').fadeIn(300);
+	
+		$('p.heroCopy'+currentSpotlight+'').siblings('p').removeClass('current').fadeOut(600);
+		$(this).parents().find('p.heroCopy'+currentSpotlight+'').addClass('current').fadeIn(600);	
 		
-	$(this).parents().find('a.prev').attr('rel', parseInt(currentSpotlight)-prev);					
-	$(this).parents().find('a.next').attr('rel', parseInt(currentSpotlight)+next);			
+		// change the next/prev				
+		var prev = 1;
+		var next = 1;
+		if(currentSpotlight == lastSpotlight)
+			next = -lastSpotlight+1;
+		if(currentSpotlight == '1')
+			prev = -lastSpotlight+1;
+			
+		$(this).parents().find('a.prev').attr('rel', parseInt(currentSpotlight)-prev);					
+		$(this).parents().find('a.next').attr('rel', parseInt(currentSpotlight)+next);			
+	});
+	
+	
+	
+	$('#SGGN1 ul li a').live('click',
+		function() {
+			var number = $(this).attr("class");
+			$('#SGGI1 img:visible').hide();
+			$('#SGGI1 img.'+number+'').fadeIn();
+	});
 });
 
 $('#SGM6 div.letter ul li a').live('click',
 	function() {
-		if($(this).parent().hasClass('selected')){
+		if($(this).parent().hasClass('selected')) {
 			$(this).parent().removeClass('selected');
-		}
-		else {
-		$(this).parent().addClass('selected');
+		} else {
+			$(this).parent().addClass('selected');
 		}
 });
