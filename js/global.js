badbb15cf32882ecdf395c5419d50b320fb34208
Daniel Cohen <dcohen2@sapient.com>
2010-07-26 16:55:29 -0400

    hero changes, ie compatibility fixes

--- js/global.js
+++ js/global.js
@@ -9,12 +9,14 @@ $('div.carousel a').live('click',
 	$('img.bgHeroSlide'+currentSpotlight+'').siblings('img.bgHero').fadeOut(600);
 	$(this).parents().find('img.bgHeroSlide'+currentSpotlight+'').fadeIn(300);
 
-	$('img.heroLeftOverlay'+currentSpotlight+'').siblings().removeClass('current').fadeOut(300);
-	$(this).parents().find('img.heroLeftOverlay'+currentSpotlight+'').fadeIn(300);
-
 	$('p.heroCopy'+currentSpotlight+'').siblings('p').removeClass('current').fadeOut(600);
-	$(this).parents().find('p.heroCopy'+currentSpotlight+'').addClass('current').fadeIn(600);	
+	$(this).parents().find('p.heroCopy'+currentSpotlight+'').addClass('current').fadeIn(600);
 	
+	if(currentSpotlight == '1')
+		$('div.right').addClass('transparent');
+	if(currentSpotlight != '1')
+		$('div.right').removeClass('transparent');
+		
 	// change the next/prev				
 	var prev = 1;
 	var next = 1;
